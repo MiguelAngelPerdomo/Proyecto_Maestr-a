0d2aea784900c427ecb069045ac10cb2eaa774be
MiguelAngelPerdomo <160289535+MiguelAngelPerdomo@users.noreply.github.com>
2025-10-08 08:23:39 -0500

    Update 01. DiagVsSoporte_Prueba.py

--- 01. DiagVsSoporte_Prueba.py
+++ 01. DiagVsSoporte_Prueba.py
@@ -3,7 +3,7 @@ import re
 from pymongo import MongoClient
 
 # 📌 1. Conectar a MongoDB
-client = MongoClient("mongodb+srv://mperdomol1:1OkwRaT9EQSLpGbh@datosincapacidades.t9vkenb.mongodb.net/?retryWrites=true&w=majority&appName=DatosIncapacidades")
+client = MongoClient("mongodb+srv://:1OkwRaT9EQSLpGbh@datosincapacidades.t9vkenb.mongodb.net/?retryWrites=true&w=majority&appName=DatosIncapacidades")
 db = client["DatosIncapacidades"]
 
 # 📌 2. Cargar datos desde MongoDB
